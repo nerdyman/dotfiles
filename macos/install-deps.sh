63ee5f543aacc86f139c65e86dd245425c0ee226
nerdyman <averynerdyman@gmail.com>
2022-05-31 17:08:26 +0100

    add dependency install script and zprofile for macos

---
+++ macos/install-deps.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+
+brew install awscli bat coreutils fzf gnu-tar grc git-delta htop jq neovim ripgrep shellcheck starship tree zsh-syntax-highlighting zstd
+brew install --cask chromium flameshot hiddenbar kap kitty raycast
+brew install --cask linearmouse --no-quarantine
