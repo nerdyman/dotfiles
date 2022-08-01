1f4084d7fadf7dc36f5cdac3e473c10b3ba5453c
nerdyman <averynerdyman@gmail.com>
2022-08-01 19:13:49 +0100

    update install-deps script to auto install oh-my-zsh, fnm, and add homebrew configs

--- install-deps.sh
+++ install-deps.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/me/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install awscli bat coreutils fzf gnu-tar grc git-delta htop jq neovim ripgrep shellcheck starship tree zsh zsh-syntax-highlighting zstd
+brew install awscli bat coreutils fnm fzf gnu-tar grc git-delta htop jq neovim ripgrep shellcheck starship tree zsh zsh-syntax-highlighting zstd
 
 # macOS
 if [[ "$(uname)" == "Darwin" ]]; then
 	brew install --cask flameshot hiddenbar kap kitty raycast
 	brew install --cask linearmouse --no-quarantine
+	softwareupdate --install-rosetta
 fi
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
