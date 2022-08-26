b856c7ccf8b76b2d2403f21035e278fa802879a3
nerdyman <averynerdyman@gmail.com>
2022-08-26 21:21:19 +0100

    update arch install configs

--- archinstall/download.sh
+++ archinstall/download.sh
@@ -6,4 +6,9 @@ curl -fsSL https://raw.githubusercontent.com/nerdyman/dotfiles/main/archinstall/
 curl -fsSL https://raw.githubusercontent.com/nerdyman/dotfiles/main/archinstall/user_credentials.json -O
 curl -fsSL https://raw.githubusercontent.com/nerdyman/dotfiles/main/archinstall/user_disk_layout.json -O
 curl -fsSL https://raw.githubusercontent.com/nerdyman/dotfiles/main/archinstall/install.sh -O
-echo "edit $(pwd)/user_credentials.json to change password, then run bash ./install.sh"
+echo "You might want to make some changes:"
+echo "=> $(pwd)/user_configuration.json to change harddrives"
+echo "=> $(pwd)/user_credentials.json to change password"
+echo "=> $(pwd)/user_disk_layout.json to change install disk key"
+
+zsh .
