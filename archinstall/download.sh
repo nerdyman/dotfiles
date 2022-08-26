934cc43318f81736ea78dadd8408c2abb18dd090
nerdyman <averynerdyman@gmail.com>
2022-08-26 20:44:14 +0100

    update archinstall scripts

--- archinstall/download.sh
+++ archinstall/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p /tmp/archinstall-configs
+cd "$_" || exit
 curl -fsSL https://raw.githubusercontent.com/nerdyman/dotfiles/main/archinstall/user_configuration.json
 curl -fsSL https://raw.githubusercontent.com/nerdyman/dotfiles/main/archinstall/user_credentials.json
 curl -fsSL https://raw.githubusercontent.com/nerdyman/dotfiles/main/archinstall/user_disk_layout.json
 echo "edit $(pwd)/user_credentials.json to change password, then run bash ./install.sh"
-popd || exit
