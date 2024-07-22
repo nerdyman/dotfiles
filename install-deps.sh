5bff12a24f4d544553ef1c50b6e4ec528a0671e1
nerdyman <averynerdyman@gmail.com>
2024-07-22 12:43:11 +0100

    replace ls+grc with eza

--- install-deps.sh
+++ install-deps.sh
@@ -4,15 +4,12 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/in
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/me/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install awscli bandwhich bash-completion bat bat-extras coreutils ctop fnm fx fzf gnu-tar grc git git-delta htop inetutils jq neovim prettier progress ripgrep shellcheck starship tree zsh zsh-syntax-highlighting zstd
+brew install awscli bandwhich bash-completion bat bat-extras coreutils ctop eza fnm fzf gnu-tar grc git git-delta htop inetutils jq neovim ripgrep shellcheck starship tree zsh zsh-syntax-highlighting zstd
 
 # macOS
 if [[ "$(uname)" == "Darwin" ]]; then
-	brew install --cask flameshot hiddenbar kap kitty raycast
+	brew install --cask nikitabobko/tap/aerospace flameshot hiddenbar kap kitty raycast
 	brew install --cask linearmouse --no-quarantine
-
-	brew install yulrizka/tap/pushtotalk
-	ln -s /opt/homebrew/opt/pushtotalk/PushToTalk.app /Applications/PushToTalk.app
 
 	brew install iproute2mac rustup-init
 
