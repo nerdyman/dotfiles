0deeba28cd14f8686755015b600d99eaed8f660e
nerdyman <averynerdyman@gmail.com>
2025-08-29 11:24:11 +0100

    [macos] disable ai and dictation, make hostname configurable

--- macos-mods.sh
+++ macos-mods.sh
@@ -5,7 +5,12 @@ setopt VERBOSE
 # @see https://daiderd.com/nix-darwin/manual/index.html
 # @see https://gist.github.com/romanhaa/9804183f242991007b316a59c4ba5e5a
 
-sudo scutil --set HostName ricbook
+FALLBACK_HOSTNAME=$(hostname)
+MOD_HOSTNAME=${1:-$FALLBACK_HOSTNAME}
+
+echo "Using hostname: $MOD_HOSTNAME"
+
+sudo scutil --set HostName "$MOD_HOSTNAME"
 
 # Reset to defaults using `defaults delete -g <FEATURE>`
 
@@ -44,6 +49,10 @@ defaults write com.apple.dock "mru-spaces" -bool "false"
 defaults write com.apple.dock expose-group-apps -bool true
 restart dock
 killall Dock
+
+# dictation
+# disable dictation
+defaults write com.apple.HIToolbox AppleDictationAutoEnable -int 0
 
 # feedback assistant
 # do not autogather large files when submitting a report
@@ -87,6 +96,10 @@ defaults write com.apple.dock expose-animation-duration -float 0.1
 # automatically quit printer app once the print jobs complete
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
+# disable Apple Intelligence
+defaults write com.apple.CloudSubscriptionFeatures.optIn "device" -bool "false"
+defaults write com.apple.CloudSubscriptionFeatures.optIn "auto_opt_in" -bool "false"
+
 # disable automatic capitalization
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 
@@ -111,9 +124,21 @@ defaults write NSGlobalDomain AppleMetricUnits -bool true
 # set the timezone; see `sudo systemsetup -listtimezones` for other values
 sudo systemsetup -settimezone "Europe/London" > /dev/null
 
-## ---
+# Disable Spotlight Indexing
+sudo mdutil -i off -d /
 
-# https://privacy.sexy — v0.11.4 — Sun, 21 Aug 2022 15:27:02 GMT
+# Disable font smoothing (looks bad on regular HD displays)
+#defaults write -g AppleFontSmoothing -int 1
+#defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+
+# Disable mouse acceleration
+defaults write .GlobalPreferences com.apple.mouse.scaling -1
+
+# Trackpad
+# disable haptic feedback
+defaults write NSGlobalDomain com.apple.trackpad.forceClick -bool false
+
+# https://privacy.sexy — v0.13.8 — Tue, 17 Jun 2025 10:42:19 GMT
 if [ "$EUID" -ne 0 ]; then
     script_path=$([[ "$0" = /* ]] && echo "$0" || echo "$PWD/${0#./}")
     sudo "$script_path" || (
@@ -124,24 +149,130 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 
-# Deactivate the Remote Management Service
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -stop
+# Disable personalized advertisements and identifier tracking
+echo '--- Disable personalized advertisements and identifier tracking'
+defaults write com.apple.AdLib allowIdentifierForAdvertising -bool false
+defaults write com.apple.AdLib allowApplePersonalizedAdvertising -bool false
+defaults write com.apple.AdLib forceLimitAdTracking -bool true
+# ----------------------------------------------------------
 
-# Remove Apple Remote Desktop Settings
-sudo rm -rf /var/db/RemoteManagement
-sudo defaults delete /Library/Preferences/com.apple.RemoteDesktop.plist
-defaults delete ~/Library/Preferences/com.apple.RemoteDesktop.plist
-sudo rm -r /Library/Application\ Support/Apple/Remote\ Desktop/
-rm -r ~/Library/Application\ Support/Remote\ Desktop/
-rm -r ~/Library/Containers/com.apple.RemoteDesktop
 
-# Disable "Ask Siri"
+# ----------------------------------------------------------
+# ------------Disable Microsoft Office telemetry------------
+# ----------------------------------------------------------
+echo '--- Disable Microsoft Office telemetry'
+defaults write com.microsoft.office DiagnosticDataTypePreference -string ZeroDiagnosticData
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ---------------Disable guest account login----------------
+# ----------------------------------------------------------
+echo '--- Disable guest account login'
+sudo defaults write '/Library/Preferences/com.apple.loginwindow' 'GuestEnabled' -bool NO
+if ! command -v 'sysadminctl' &> /dev/null; then
+    echo 'Skipping because "sysadminctl" is not found.'
+else
+    sudo sysadminctl -guestAccount off
+fi
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -----------Disable guest file sharing over SMB------------
+# ----------------------------------------------------------
+echo '--- Disable guest file sharing over SMB'
+sudo defaults write '/Library/Preferences/SystemConfiguration/com.apple.smb.server' 'AllowGuestAccess' -bool NO
+if ! command -v 'sysadminctl' &> /dev/null; then
+    echo 'Skipping because "sysadminctl" is not found.'
+else
+    sudo sysadminctl -smbGuestAccess off
+fi
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -----------Disable guest file sharing over AFP------------
+# ----------------------------------------------------------
+echo '--- Disable guest file sharing over AFP'
+sudo defaults write '/Library/Preferences/com.apple.AppleFileServer' 'guestAccess' -bool NO
+if ! command -v 'sysadminctl' &> /dev/null; then
+    echo 'Skipping because "sysadminctl" is not found.'
+else
+    sudo sysadminctl -afpGuestAccess off
+fi
+sudo killall -HUP AppleFileServer
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -------------Disable online spell correction--------------
+# ----------------------------------------------------------
+echo '--- Disable online spell correction'
+defaults write NSGlobalDomain WebAutomaticSpellingCorrectionEnabled -bool false
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ---------------Disable remote Apple events----------------
+# ----------------------------------------------------------
+echo '--- Disable remote Apple events'
+sudo systemsetup -setremoteappleevents off
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# --Disable automatic storage of documents in iCloud Drive--
+# ----------------------------------------------------------
+echo '--- Disable automatic storage of documents in iCloud Drive'
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ---------------Disable AirDrop file sharing---------------
+# ----------------------------------------------------------
+echo '--- Disable AirDrop file sharing'
+defaults write com.apple.NetworkBrowser DisableAirDrop -bool true
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ----------------Disable Spotlight indexing----------------
+# ----------------------------------------------------------
+echo '--- Disable Spotlight indexing'
+sudo mdutil -i off -d /
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ------Disable participation in Siri data collection-------
+# ----------------------------------------------------------
+echo '--- Disable participation in Siri data collection'
+defaults write com.apple.assistant.support 'Siri Data Sharing Opt-In Status' -int 2
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# --------------------Disable "Ask Siri"--------------------
+# ----------------------------------------------------------
+echo '--- Disable "Ask Siri"'
 defaults write com.apple.assistant.support 'Assistant Enabled' -bool false
+# ----------------------------------------------------------
 
-# Disable Siri voice feedback
+
+# ----------------------------------------------------------
+# ---------------Disable Siri voice feedback----------------
+# ----------------------------------------------------------
+echo '--- Disable Siri voice feedback'
 defaults write com.apple.assistant.backedup 'Use device speaker for TTS' -int 3
+# ----------------------------------------------------------
 
-# Disable Siri services (Siri and assistantd)
+
+# ----------------------------------------------------------
+# -------Disable Siri services (Siri and assistantd)--------
+# ----------------------------------------------------------
+echo '--- Disable Siri services (Siri and assistantd)'
 launchctl disable "user/$UID/com.apple.assistantd"
 launchctl disable "gui/$UID/com.apple.assistantd"
 sudo launchctl disable 'system/com.apple.assistantd'
@@ -151,42 +282,29 @@ sudo launchctl disable 'system/com.apple.Siri.agent'
 if [ $(/usr/bin/csrutil status | awk '/status/ {print $5}' | sed 's/\.$//') = "enabled" ]; then
     >&2 echo 'This script requires SIP to be disabled. Read more: https://developer.apple.com/documentation/security/disabling_and_enabling_system_integrity_protection'
 fi
+# ----------------------------------------------------------
 
-# Disable use Siri prompt
+
+# ----------------------------------------------------------
+# -------Disable "Do you want to enable Siri?" pop-up-------
+# ----------------------------------------------------------
+echo '--- Disable "Do you want to enable Siri?" pop-up'
 defaults write com.apple.SetupAssistant 'DidSeeSiriSetup' -bool True
+# ----------------------------------------------------------
 
-# Hide Siri from menu bar
+
+# ----------------------------------------------------------
+# ----------------Remove Siri from menu bar-----------------
+# ----------------------------------------------------------
+echo '--- Remove Siri from menu bar'
 defaults write com.apple.systemuiserver 'NSStatusItem Visible Siri' 0
+# ----------------------------------------------------------
 
-# Hide Siri from status menu
+
+# ----------------------------------------------------------
+# ---------------Remove Siri from status menu---------------
+# ----------------------------------------------------------
+echo '--- Remove Siri from status menu'
 defaults write com.apple.Siri 'StatusMenuVisible' -bool false
 defaults write com.apple.Siri 'UserHasDeclinedEnable' -bool true
-
-# Disable Siri telemetry
-defaults write com.apple.assistant.support 'Siri Data Sharing Opt-In Status' -int 2
-
-# Disable Remote Apple Events
-sudo systemsetup -setremoteappleevents off
-
-# Do not store docs on iCloud by default
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-# Disable AirDrop
-defaults write com.apple.NetworkBrowser DisableAirDrop -bool true
-
-
-# Disable Spotlight Indexing
-sudo mdutil -i off -d /
-
-## ---
-
-# Disable font smoothing (looks bad on regular HD displays)
-defaults write -g AppleFontSmoothing -int 1
-defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
-
-# Disable mouse acceleration
-defaults write .GlobalPreferences com.apple.mouse.scaling -1
-
-# Trackpad
-# disable haptic feedback
-defaults write NSGlobalDomain com.apple.trackpad.forceClick -bool false
+# ----------------------------------------------------------
