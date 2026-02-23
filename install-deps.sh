3df16f97c2881f03384a1fa6bcf80043b6737525
nerdyman <averynerdyman@gmail.com>
2026-02-23 19:57:30 +0000

    [install-deps] add direnv, alt-tab coteditor and monitorcontrol

--- install-deps.sh
+++ install-deps.sh
@@ -4,13 +4,13 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/in
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/me/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install awscli bandwhich bash-completion bat bat-extras coreutils ctop eza fnm fzf gnu-tar grc git git-delta htop inetutils jq neovim ripgrep shellcheck starship tree zsh zsh-syntax-highlighting zstd
+brew install awscli bandwhich bash-completion bat bat-extras coreutils ctop direnv eza fnm fzf gnu-tar grc git git-delta htop inetutils jq neovim ripgrep shellcheck starship tree zsh zsh-syntax-highlighting zstd
 
 # macOS
 if [[ "$(uname)" == "Darwin" ]]; then
 	brew tap FelixKratz/formulae
-	brew install --cask nikitabobko/tap/aerospace flameshot hiddenbar kap raycast wezterm
-	brew install --cask linearmouse --no-quarantine
+	brew install --cask nikitabobko/tap/aerospace alt-tab coteditor flameshot kap monitorcontrol raycast wezterm
+	brew install --cask hiddenbar linearmouse --no-quarantine
 
 	brew install borders iproute2mac rustup-init
 
